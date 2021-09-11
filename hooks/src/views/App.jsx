cc67430e3352b94fb7fd42eeb43dcb897f40e5cc
yodono <yodono.victor@gmail.com>
2021-09-11 20:04:40 +0000

    Wrap Menu and Content components in a Store context

--- hooks/src/views/App.jsx
+++ hooks/src/views/App.jsx
@@ -3,14 +3,17 @@ import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
+import Store from '../data/Store'
 
 const App = props => (
-    <div className="App">
-        <Router>
-            <Menu />
-            <Content />
-        </Router>
-    </div>
+    <Store>
+        <div className="App">
+            <Router>
+                <Menu />
+                <Content />
+            </Router>
+        </div>
+    </Store>
 )
 
 export default App
